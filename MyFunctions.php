abf51936ca7f2a0f3e05edfa3acf08bd1b55e12f
Alex Dargelis <alex@dargelis.net>
2020-05-02 19:56:04 +0300

    access trick1

--- MyFunctions.php
+++ MyFunctions.php
@@ -1,9 +1,11 @@
 <?php
 
-define('DB_SERVER', '10.196.68.11');
-define('DB_USERNAME', 'AlexSys');
-define('DB_PASSWORD', 'crystal');
-define('DB_NAME', 'AlexSys');
+require_once "config\access.php";
+// define('DB_SERVER', '10.#################');
+// define('DB_USERNAME', '.#################');');
+// define('DB_PASSWORD', '.#################');');
+// define('DB_NAME', '.#################');');
+
 
 
 //check connection to DB
